a70246bf598628c97da3b697fcd8e8ac1991a490
Albert Michael Byrd <alxp2083@hotmail.com>
2022-05-25 01:48:49 -0400

    Redo Update

--- scripts/page.js
+++ scripts/page.js
@@ -1,36 +1,4 @@
-const mynav = document.querySelector("#mynav");
 
-//Write your Navgation here
-mynav.innerHTML += "<a href='index.html' title='ABMedia83 Home Page' class='home' ></a>"+
-"<div class='thenav'>"+
-"<ul>"+
-"<li><a href='about.html' title='About ABMedia83'>about</a></li>"+
-"<li><a href='portfolio.html' title='Portfolio'>portfolio</a>"+
-"<li><a href='video.html' title='Video'>video</a>"+
-"<li><a href='resources.html' title='Resources'>resources</a>"+
-"<li><a href='contact.html' title='Contact'>contact</a>"+
-"</ul>"+
-"</div> \n"+
-"<div class='mnu' title='Menu' >"+
-"<div class='bar'></div>"+
-"<div class='bar'></div>"+
-"<div class='bar'></div>"+
-"</div>";
-
-
-
-
-
-
-
-//Get Menu Button 
-let menu = document.querySelector(".mnu"); 
-//Get the Navigation Bar 
-let nav = document.querySelector(".thenav"); 
-//Add Menu Event Listner 
-menu.addEventListener("click", function() 
-{
-    menu.classList.toggle("active");
-    nav.classList.toggle("active");
-
-})
+//Setup your navigation here 
+let mynav =  document.querySelector("#mynav");
+mynav.innerHTML = `${+"<div class='mnu'>"}<div class='bar'></div><div class='bar'></div><div class='bar'></div></div><div class='thenav'><ul><li><a href='portfolio.html' title='ABMedia83 Portfolio'>portfolio</a></li><li><a href='video.html' title='ABMedia83 Video'>video</a></li><li><a href='resources.html' title='ABMedia83 Resources'>resourcs</a></li><li><a href='contact.html' title='Contact ABMedia83'>contact</a></li></ul></div>`;
