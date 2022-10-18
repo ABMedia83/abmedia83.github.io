2602335641cfe5e3203b5fa75d6e7ee9fd0715eb
Albert Michael Byrd <alxp2083@hotmail.com>
2022-10-18 17:04:59 -0400

    Rrefresh

--- scripts/page.js
+++
@@ -1,4 +0,0 @@
-
-//Setup your navigation here 
-let mynav =  document.querySelector("#mynav");
-mynav.innerHTML = `${+"<div class='mnu'>"}<div class='bar'></div><div class='bar'></div><div class='bar'></div></div><div class='thenav'><ul><li><a href='portfolio.html' title='ABMedia83 Portfolio'>portfolio</a></li><li><a href='video.html' title='ABMedia83 Video'>video</a></li><li><a href='resources.html' title='ABMedia83 Resources'>resourcs</a></li><li><a href='contact.html' title='Contact ABMedia83'>contact</a></li></ul></div>`;
